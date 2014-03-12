de1f9f389705bb24a3c45a1359512da0f35e941c
tylucaskelley <tylucaskelley@gmail.com>
2014-03-11 23:43:55 -0400

    Problems 4 and 5 solved

---
+++ Source Code/ProblemFive.cpp
@@ -0,0 +1,29 @@
+//Problem Five: Smallest number that can be divided by all integers 1 - 20 w/o remainder
+
+#include <iostream>
+using namespace std;
+
+static int START = 2520;
+
+bool smallest_multiple(int x);
+
+int main(int argc, char *argv[]) {
+  int i = START;
+  while (true) {
+    if (smallest_multiple(i)) {
+      break;
+    }
+    i += 20;
+  }
+  cout << i << endl;
+}
+
+bool smallest_multiple(int x) {
+  int i = 11;
+  for (i; i <= 20; i++) {
+    if (x % i != 0) {
+      return false;
+    }
+  }
+  return true;
+}
