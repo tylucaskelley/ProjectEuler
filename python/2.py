212533d52b24b7474f44e564f13119753ccb2395
Ty-Lucas Kelley <tylucas.kelley@hasbro.com>
2014-09-16 15:55:17 -0400

    renamed python var sum to s

--- python/2.py
+++ python/2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 def fib_sum(n):
-    sum, a, b = (0, 1, 2)
+    s, a, b = (0, 1, 2)
     while a <= n:
         if a % 2 == 0:
-            sum += a
+            s += a
         a, b = b, a + b
-    return sum
+    return s
 
 print fib_sum(4000000)
